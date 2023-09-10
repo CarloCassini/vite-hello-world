17e722c649dcb40a80e02287c5cd4317db072e1c
CarloCassini <c.cassini87@gmail.com>
2023-09-10 16:49:22 +0200

    add jumbothrone

--- vite-project/src/App.vue
+++ vite-project/src/App.vue
@@ -1,14 +1,17 @@
 <script>
 import AppTitle from "./components/AppTitle.vue";
+import AppJumb from "./components/AppJumb.vue";
 
 export default {
   components: {
     AppTitle: AppTitle,
+    AppJumb: AppJumb,
   },
 };
 </script>
 
 <template>
+  <AppJumb />
   <AppTitle />
 </template>
 
---
+++ vite-project/src/components/AppJumb.vue
@@ -0,0 +1,15 @@
+<script>
+export default {
+  data() {
+    return {
+      image: "../img/pc_ver_1.png",
+    };
+  },
+};
+</script>
+
+<template>
+  <img :src="image" alt="" />
+</template>
+
+<style></style>
